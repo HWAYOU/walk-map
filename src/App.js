e86e6e61b07b1b06315145b1db353307361dc6ea
hwayoung <hyriver37@gamil.com>
2023-10-16 17:45:29 +0900

    [Feat] 라우터변경(RouterProvider index파일로)

--- src/App.js
+++ src/App.js
@@ -1,42 +1,32 @@
 import "./App.scss";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HealthAdd from "./component/HealthAdd";
-import NotFound from "./component/NotFound";
-import HealthList from "./component/HealthList";
-import Main from "./component/Main";
-import Root from "./component/Root";
+import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import Navigation from "./component/Navigation";
+import { getDocs } from "firebase/firestore";
+
 import { db } from "./firebase";
 import { collection } from "firebase/firestore";
-import HealthInfo from "./component/HealthInfo";
-//
-//firebase db의 collection인 fruits를 연결하기
-const healthRef = collection(db, "health");
 
-//라우터 생성
-//healthRef={healthRef} : 각각의 컴포넌트에 데이터 연결하기
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, element: <Main /> },
-      { path: "/healthadd", element: <HealthAdd healthRef={healthRef} /> },
-      { path: "/healthlist", element: <HealthList healthRef={healthRef} /> },
-      {
-        path: "/healthlist/:healthId",
-        element: <HealthInfo healthRef={healthRef} />,
-      },
-    ],
-  },
-]);
+//firebase db의 collection인 fruits를 연결하기
+// const data = collection(db, "health");
+// const data = await getDocs(healthRef);
+// console.log(db);
+
+export const HealthStateContext = React.createContext();
 
 //라우터 적용
 const App = () => {
+  useEffect(() => {
+    console.log(db);
+  });
   return (
+    // <HealthStateContext.Provider value={data}>
     <div className="app">
-      <RouterProvider router={router} />
+      <Navigation />
+      <Outlet />
+      {/* Outlet : 중첩된 라우팅 구조에서 부모 라우트 컴포넌트 내에서 자식 라우트 컴포넌트를 렌더링하기 위해 사용 */}
     </div>
+    // </HealthStateContext.Provider>
   );
 };
 
---
+++ src/Router.jsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import App from "./App";
+import Main from "./component/Main";
+import HealthAdd from "./component/HealthAdd";
+import HealthInfo from "./component/HealthInfo";
+import HealthList from "./component/HealthList";
+import NotFound from "./component/NotFound";
+
+import { db } from "./firebase";
+import { collection } from "firebase/firestore";
+
+//firebase db의 collection인 fruits를 연결하기
+const healthRef = collection(db, "health");
+
+//✅라우터 생성
+//createBrowserRouter를 사용하는 이유? data api 사용(errorElement)
+//healthRef={healthRef} : 각각의 컴포넌트에 데이터 연결하기 -> 이걸 context api로 가져올 수 있지 않나? props drilling 지양
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Main /> }, //index: true -> 해당경로에 대한 기본페이지. 즉 '/' 일때 <Main /> 렌더링
+      { path: "/healthadd", element: <HealthAdd healthRef={healthRef} /> },
+      { path: "/healthlist", element: <HealthList healthRef={healthRef} /> },
+      {
+        path: "/healthlist/:healthId",
+        element: <HealthInfo healthRef={healthRef} />,
+      },
+    ],
+  },
+]);
+
+export default router;
