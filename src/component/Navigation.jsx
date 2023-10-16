34bd18896297fbde83481be484dbf9c840d01efe
hwayoung <hyriver37@gamil.com>
2023-10-16 17:43:57 +0900

    [Rename] 컴포넌트 명 변경(main -> nevigation)

--- src/component/Navigation.jsx
+++ src/component/Navigation.jsx
@@ -1,36 +1,39 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "../style/Menu.scss";
+import "../style/Navigation.scss";
 
-const Menu = () => {
+const Navigation = () => {
   const activeStyle = {
     color: "#6392ff",
   };
 
   return (
-    <div className="menu">
+    <div className="Navigation">
       <NavLink
         className="link"
         style={({ isActive }) => (isActive ? activeStyle : {})}
-        to="/">
+        to="/"
+      >
         메인
       </NavLink>
 
       <NavLink
         className="link"
         style={({ isActive }) => (isActive ? activeStyle : {})}
-        to="/healthadd">
+        to="/healthadd"
+      >
         건강기록
       </NavLink>
 
       <NavLink
         className="link"
         style={({ isActive }) => (isActive ? activeStyle : {})}
-        to="/healthlist">
+        to="/healthlist"
+      >
         기록관리
       </NavLink>
     </div>
   );
 };
 
-export default Menu;
+export default Navigation;
