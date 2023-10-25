98f34f8200b08cbc83bd832d9843fa286df6bc13
hwayoung <hyriver37@gamil.com>
2023-10-25 22:52:06 +0900

    [Feat] List 페이지 구현

--- src/component/HealthItem.jsx
+++ src/component/HealthItem.jsx
@@ -1,9 +1,31 @@
 import React from "react";
-import "../style/component/HealthItem.scss";
+import { useNavigate } from "react-router-dom";
+import "../style/page/List.scss";
 
-const HealthItem = () => {
-  // return (
-  // );
+const HealthItem = ({ hid, exercise, time, sleep, water, mind }) => {
+  const navigate = useNavigate();
+
+  //item 클릭 시 detail 페이지로 이동
+  const clickHandler = (id) => {
+    navigate(`/List/${id}`);
+  };
+
+  return (
+    <tr
+      key={hid}
+      className="healthTr"
+      onClick={() => {
+        clickHandler(hid);
+      }}
+    >
+      <td className="hidTd">{hid}</td>
+      <td>{exercise}</td>
+      <td>{time}</td>
+      <td>{sleep}</td>
+      <td>{water}</td>
+      <td className="mindTd">{mind}</td>
+    </tr>
+  );
 };
 
 export default HealthItem;
--- src/component/HealthList.jsx
+++ src/component/HealthList.jsx
@@ -1,9 +1,25 @@
 import React from "react";
-import "../style/component/HealthList.scss";
+import HealthItem from "./HealthItem";
+import "../style/page/List.scss";
 
-const HealthList = () => {
-  // return (
-  // );
+const HealthList = ({ healthList }) => {
+  return (
+    <table className="healthTable">
+      <tbody>
+        <tr className="healthToptr">
+          <th>일자</th>
+          <th>운동</th>
+          <th>운동시간</th>
+          <th>수면시간</th>
+          <th>수분섭취(ml)</th>
+          <th className="thMind">마음건강</th>
+        </tr>
+        {healthList.map((it) => (
+          <HealthItem key={it.hid} {...it} />
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default HealthList;
