89340ffa3dbb02bc31d89143a34de1aa89bb5b24
hwayoung <hyriver37@gamil.com>
2023-10-31 13:39:37 +0900

    [Feat] notfound 페이지 버튼 추가

--- src/page/NotFound.jsx
+++ src/page/NotFound.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "../style/page/NotFound.scss";
+import MyButton from "../component/MyButton";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  const clickHandler = () => {
+    navigate("/");
+  };
   return (
     <div className="NotFound">
       <h1>404</h1>
       <h1>페이지를 찾을 수 없습니다.</h1>
+      <MyButton type={"positive"} text={"홈으로"} onClick={clickHandler} />
     </div>
   );
 };
